2ff65d21d64ea3d1ddf83b6cabefaf6bdbe24af9
nathanielalex <nathanalex019@gmail.com>
2025-07-27 18:29:34 +0700

    init flask

---
+++ AI-Services/main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+
+app = Flask(__name__)
+
+cors = CORS(app, origins='*')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
